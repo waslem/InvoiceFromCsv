a431af698c8cec5920e36d00af233fee54a00fc5
Jamie van Walsum <jvw@westnet.com.au>
2014-09-04 09:21:14 +0800

    added files

---
+++ invoiceFromCSV/invoiceFromCSV/Application.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+
+namespace invoiceFromCSV
+{
+    class Application : ServiceBase
+    {
+        public static void Main(string[] args)
+        {
+            ServiceBase.Run(new Application());
+        }
+
+        public Application()
+        {
+            this.ServiceName = "csv invoice service";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+    }
+}
---
+++ invoiceFromCSV/invoiceFromCSV/invoiceServiceInstaller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration.Install;
+using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace invoiceFromCSV
+{
+    [RunInstaller(true)]
+    public class invoiceServiceInstaller : Installer
+    {
+        public invoiceServiceInstaller()
+        {
+            var processInstaller = new ServiceProcessInstaller();
+            var serviceInstaller = new ServiceInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+
+            serviceInstaller.DisplayName = "csv invoice service";
+            serviceInstaller.StartType = ServiceStartMode.Manual;
+
+            //must be the same as what was set in Program's constructor
+            serviceInstaller.ServiceName = "csv invoice service";
+
+            this.Installers.Add(processInstaller);
+            this.Installers.Add(serviceInstaller);
+
+
+        }
+    }
+}
